d65c844980fe58d56c8e553b26f1ad99f6572727
nini-netizen <nini0905763217@gmail.com>
2026-05-22 11:39:20 +0800

    add mut_func

--- src/calculate.py
+++ src/calculate.py
@@ -1,2 +1,10 @@
 def add_func(a, b):
     return a + b
+
+
+def sub_func(a, b):
+    return a - b
+
+
+def mut_func(a: float, b: float) -> float:
+    return a * b
--- test/test_calculate.py
+++ test/test_calculate.py
@@ -1,7 +1,19 @@
-from calculate import add_func
+from calculate import add_func, mut_func, sub_func
 
 
 def test_add_func():
     assert add_func(1, 2) == 3
     assert add_func(0, 0) == 0
     assert add_func(-1, -2) == -3
+
+
+def test_sub_func():
+    assert sub_func(2, 1) == 1
+    assert sub_func(0, 0) == 0
+    assert sub_func(-2, -1) == -1
+
+
+def test_mut_func():
+    assert mut_func(2, 3) == 6
+    assert mut_func(0, 5) == 0
+    assert mut_func(-2, 3) == -6
